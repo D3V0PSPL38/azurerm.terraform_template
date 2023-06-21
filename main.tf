53572d2402faeb5b0870ae3cef7d83b886bf8f8c
RalphBrynard <ralph@thebrynards.com>
2023-06-20 20:10:26 -0500

    Title: label module
    Body:
    
    Co-authored-by: Ralph Brynard <ralph@thebrynards.com>

--- main.tf
+++ main.tf
@@ -1,2 +1,15 @@
 ### [BEGIN] main.tf ###
+module "label" {
+  source          = "github.com/D3V0PSPL38/terraform-context-label.git"
+  enabled         = module.this.enabled
+  name            = var.name
+  namespace       = var.namespace
+  environment     = var.environment
+  stage           = var.stage
+  location        = var.location
+  label_order     = var.label_order
+  id_length_limit = var.id_length_limit
+  tags            = var.tags
+  context         = module.this.context
+}
 ### [END] main.tf ###
